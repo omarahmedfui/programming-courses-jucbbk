6135d0fb66d06d1892b0088d86f601988ced97fb
omarahmedfui <ahmedamin0389@gmail.com>
2021-01-20 02:24:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-jucbbk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-jucbbk)
---
+++ index.ts
@@ -0,0 +1,28 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let rum1: number;
+let rum2: number;
+let rum3: number;
+let rum4: number;
+let rum5: number;
+rum1 = 12;
+rum2 = 47;
+rum3 = 800;
+rum4 = 200;
+console.log(rum1);
+console.log(rum2);
+console.log(rum1 + rum2);
+rum5 = rum1 * rum2 * rum3 * rum4;
+console.log(rum5);
+rum1 = 35 + rum1;
+rum2 = rum2 - 35;
+console.log(rum1);
+console.log(rum2);
+console.log("1", rum1);
+rum3 = rum1;
+rum1 = rum2;
+rum3 = rum2;
+console.log(rum1);
+console.log(rum2);
